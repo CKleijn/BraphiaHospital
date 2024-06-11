459aa436a76fc48292bd4aa2cc78ed88ce5b426f
GuusvD <94607611+GuusvD@users.noreply.github.com>
2024-06-11 12:58:11 +0200

    + Add System.Data.SqlClient package and unfinished util classes for db connection and event interaction

---
+++ Consultancy/Consultancy/Utils/ConfigurationHelper.cs
@@ -0,0 +1,14 @@
+﻿namespace Consultancy.Utils
+{
+    public static class ConfigurationHelper
+    {
+        public static string GetConnectionString()
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.Development.json")
+                .Build();
+
+            return configuration.GetConnectionString("EventStoreConnectionString")!;
+        }
+    }
+}
---
+++ Consultancy/Consultancy/Utils/DatabaseEventOperations.cs
@@ -0,0 +1,37 @@
+﻿using System.Data.SqlClient;
+
+namespace Consultancy.Utils
+{
+    public static class DatabaseEventOperations
+    {
+        public static void ReadEvents()
+        {
+            using SqlConnection connection = new(ConfigurationHelper.GetConnectionString());
+
+            connection.Open();
+
+            string query = "SELECT * FROM Events";
+
+            using SqlCommand command = new(query, connection);
+            using SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read()) Console.WriteLine(reader.GetString(1));
+
+            connection.Close();
+        }
+
+        public static void AddEvent(string eventValue)
+        {
+            using SqlConnection connection = new(ConfigurationHelper.GetConnectionString());
+
+            connection.Open();
+
+            string query = "INSERT INTO Events (Event) VALUES (@EventValue)";
+
+            using SqlCommand command = new(query, connection);
+            command.Parameters.AddWithValue("@EventValue", eventValue);
+
+            connection.Close();
+        }
+    }
+}
