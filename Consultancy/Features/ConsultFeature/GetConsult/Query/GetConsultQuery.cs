f6864c071d3b9e72d81d1eb2441d048eeaa53cfd
GuusvD <94607611+GuusvD@users.noreply.github.com>
2024-06-18 20:14:26 +0200

    + Add get consult functionality

---
+++ Consultancy/Features/ConsultFeature/GetConsult/Query/GetConsultQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+using Consultancy.Common.Entities;
+
+namespace Consultancy.Features.ConsultFeature.GetConsult.Query
+{
+    public sealed record GetConsultQuery(Guid id)
+        : IRequest<Consult>;
+}
